72b9050f2bf62fefce1707fca3532db8dd864639
cjy <526432451@qq.com>
2018-05-17 17:55:09 +0800

    update cms-dao，update cms-service

--- cms/cms-dao/src/main/java/com/cjy/cms/mapper/UserMapper.java
+++ cms/cms-dao/src/main/java/com/cjy/cms/mapper/UserMapper.java
@@ -2,9 +2,12 @@ package com.cjy.cms.mapper;
 
 import com.cjy.cms.model.User;
 import com.cjy.cms.model.UserExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserMapper {
     int countByExample(UserExample example);
 
